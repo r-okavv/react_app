16fab7923ac090256610c6603ef72299dc6991a7
r-okavv <ooxxbookxxoo@gmail.com>
2023-10-04 12:27:22 +0900

    Add:models/User.jsを追加、MongoDBのURLを追加

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const authRoute = require("./routes/auth")
 const postsRoute = require("./routes/posts")
 // PORT番号の指定
 const PORT = 3000;
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+// connect関数を使用してmongoDBに接続する
+mongoose.connect(
+  process.env.MONGOURL
+).then(()=>{
+  console.log("DBと接続中");
+}).catch((err)=>{
+  console.log(err);
+});
 
 // ミドルウェア
 app.use("/api/users", userRoute);
