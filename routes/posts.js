a3f708e398beb1ed1e7ae8ebfcf6956169792ea8
r-okavv <ooxxbookxxoo@gmail.com>
2023-10-04 20:39:00 +0900

    Aed:投稿のいいね機能を追加

--- routes/posts.js
+++ routes/posts.js
@@ -55,5 +55,28 @@ router.get("/:id",async(req, res)=>{
     }
 });
 
+// 投稿のいいね機能、いいね解除機能
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      //まだlikesにuserIdが含まれていない場合
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ 
+            $push: { likes: req.body.userId }
+        });
+        return res.status(200).json("post has been liked");
+        //すでにpostのlikesにuserIdが含まれている場合
+      } else {
+        await post.updateOne({ 
+            $pull: { likes: req.body.userId } 
+        });
+        return res.status(200).json("post has been disliked");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+
 // server.jsで使用するためexportする
 module.exports = router;
