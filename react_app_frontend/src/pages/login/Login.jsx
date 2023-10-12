c7f5ba2f4c7bbbdaa4f3ffdf89dad89df5257330
r-okavv <ooxxbookxxoo@gmail.com>
2023-10-13 01:00:07 +0900

    Add:ログインフォーム、登録フォームにvalidation追加

--- react_app_frontend/src/pages/login/Login.jsx
+++ react_app_frontend/src/pages/login/Login.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
+import { useRef } from 'react';
 import "./Login.css"
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  // ログインボタンをクリックした際にリロードされると入力された値が消えてしまうためイベントを取得しpreventDefaultを使用する
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(email.current.value);
+  };
+
   return (
     <div className='login'>
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 lassName="loginLogo" >React App</h3>
+          <h3 className="loginLogo" >React App</h3>
           <span className="loginDesc">キャッチコピー</span>
         </div>
-        <div className="loginRight">
-          <div className="loginBox">
+        <div className="loginRight" onSubmit={(e)=>handleSubmit(e)}>
+          <form className="loginBox">
             <p className="loginMsg">Login</p>
-            <input type="text" className="loginInput" placeholder='E-mail'/>
-            <input type="text" className="loginInput" placeholder='Password'/>
+            <input 
+              type="email" 
+              className="loginInput" 
+              placeholder='E-mail'
+              required
+              ref={email}
+              />
+            <input 
+              type="password" 
+              className="loginInput" 
+              placeholder='Password'
+              required minLength="6"
+              ref={password}/>
             <button className="loginButton">Login</button>
             <span className="loginForgot">Forgot password?</span>
             <button className="loginRegisterButton">Create New Account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- react_app_frontend/src/pages/register/Register.jsx
+++ react_app_frontend/src/pages/register/Register.jsx
@@ -6,16 +6,16 @@ export default function Register() {
     <div className='login'>
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 lassName="loginLogo" >React App</h3>
+          <h3 className="loginLogo" >React App</h3>
           <span className="loginDesc">キャッチコピー</span>
         </div>
         <div className="loginRight">
           <div className="loginBox">
             <p className="loginMsg">SignUp</p>
             <input type="text" className="loginInput" placeholder='User Name'/>
-            <input type="text" className="loginInput" placeholder='E-mail'/>
-            <input type="text" className="loginInput" placeholder='Password'/>
-            <input type="text" className="loginInput" placeholder='Password Confirmation'/>
+            <input type="email" className="loginInput" placeholder='E-mail'required/>
+            <input type="password" className="loginInput" placeholder='Password' required/>
+            <input type="password" className="loginInput" placeholder='Password Confirmation' required/>
             <button className="loginButton">Submit</button>
             <button className="loginRegisterButton">Login</button>
           </div>
