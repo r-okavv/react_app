c0c918413712a12ce699e5993075e4eb070ee1eb
r-okavv <ooxxbookxxoo@gmail.com>
2023-10-15 20:09:40 +0900

    Add:投稿機能の追加

--- react_app_frontend/src/components/share/Share.jsx
+++ react_app_frontend/src/components/share/Share.jsx
@@ -4,19 +4,42 @@ import ImageIcon from '@mui/icons-material/Image';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import GifIcon from '@mui/icons-material/Gif';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import { useContext } from 'react';
+import { AuthContext } from '../../state/AuthContext';
+import { useRef } from 'react';
+import axios from 'axios';
 
 export const Share = () => {
+  const {user} = useContext(AuthContext);
+  const desc = useRef();
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    try{
+      await axios.post("/posts", newPost);
+      window.location.reload();
+    }catch(err){
+      console.log(err);
+    }
+  };
+
   return (
   <div className='share'>
     <div className="shareWrapper">
       <div className="shareTop">
         {/* <img src={user.profilePicture || "/assets/person/noAvatar.png"} alt="" className='shareProfileImg'/> */}
         <img src="/assets/person/noAvatar.png" alt="" className='shareProfileImg'/>
-        <input type="text" className='shareInput' placeholder="What's happning?"/>
+        <input type="text" className='shareInput' placeholder="What's happning?" ref={desc}/>
       </div>
       <hr className="shareHr" />
 
-      <div className="shareButtons">
+      <form className="shareButtons" onSubmit={(e)=> handleSubmit(e)}>
         <div className="shareOptions">
           <div className="shareOption">
             <ImageIcon className='shareIcon' htmlColor="#00838f"/>
@@ -35,8 +58,8 @@ export const Share = () => {
             <span className="shareOptionText">投票</span>
           </div>
         </div>
-        <button className='shareButton'>投稿</button>
-      </div>
+        <button className='shareButton' type="submit">投稿</button>
+      </form>
     </div>
   </div>
  )
